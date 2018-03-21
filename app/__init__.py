1aea057e3a1d9f573ef96cda275583378f4e762b
bandris342 <andras.bartok@gmail.com>
2018-03-21 17:08:17 +0100

    Create admin page

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
-from app.models import db, User
+from app.models import db, User, Exercices
 from flask_login import LoginManager
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -8,6 +10,10 @@ app.config.from_object('config')
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
+admin = Admin(app, name='Admin page', template_mode='bootstrap3')
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Exercices, db.session))
 
 @login_manager.user_loader
 def load_user(id):
